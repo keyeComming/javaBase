c33c6f77424000d382b01ab736d9af9ff12c783a
yangzhangke <1426061489@qq.com>
2024-09-08 10:31:05 +0800

    测试提交

--- src/main/java/com/Test1.java
+++ src/main/java/com/Test1.java
@@ -2,6 +2,9 @@ package com;
 
 class Test1 {
     public static void main(String[] args) {
+        if (1==1 && 2==2 && 3==4 && 5==5&& 5==5&& 5==5&& 5==5&& 5==5){
+            System.out.println("true");
+        }
         System.out.println("2105200326");
         System.out.println("杨章科");
     }
